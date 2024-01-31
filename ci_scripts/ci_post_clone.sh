4192fbb73ec458f6a2e89ce1c8a44dfb0ad051c9
kelvinharron <kelvinharron@outlook.com>
2024-01-31 22:02:35 +0000

    remove ~/.local/bin/ from mise

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -2,12 +2,12 @@
 curl https://mise.jdx.dev/install.sh | sh
 export PATH="$HOME/.local/bin/$PATH" # Installs the tools in .mise.toml in the project root
 
-~/.local/bin/mise --version
-~/.local/bin/mise install # Installs the version from .mise.toml
-eval "$(~/.local/bin/mise activate bash --shims)" # activate shims to enable local use of mise
+mise --version
+mise install # Installs the version from .mise.toml
+eval "$(mise activate bash --shims)" # activate shims to enable local use of mise
 
 mise doctor # verify the output of mise is correct on CI
 
 cd ..
-~/.local/bin/mise x -- tuist fetch
-~/.local/bin/mise x -- tuist generate # Generate the Xcode Project using Tuist
+mise x -- tuist fetch
+mise x -- tuist generate # Generate the Xcode Project using Tuist
--- ci_scripts/generate-mocks.sh
+++ ci_scripts/generate-mocks.sh
@@ -6,4 +6,4 @@ echo "Current working directory: $current_path"
 relative_root_path="./Targets/TuistXcodeCloud"
 relative_path="./Targets/TuistXcodeCloud/Tests/GeneratedMocks.swift"
 
-~/.local/bin/mise x -- mockolo -s $relative_root_path -d $relative_path -i TuistXcodeCloud
+mise x -- mockolo -s $relative_root_path -d $relative_path -i TuistXcodeCloud
